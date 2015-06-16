7d3339da2b54fcef43cac25f8e85bb0a10cbb316
∂jalel <djalel.benbouzid@gmail.com>
2015-06-16 16:02:26 +0200

    Random forests with random embedding

--- regressor.py
+++ regressor.py
@@ -1,10 +1,21 @@
-from sklearn.linear_model import LinearRegression
 from sklearn.base import BaseEstimator
- 
+from sklearn.ensemble import RandomTreesEmbedding
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import KernelPCA, PCA
+from sklearn.gaussian_process import GaussianProcess
+
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.reg = LinearRegression()
+        rte = RandomTreesEmbedding(n_estimators=100, n_jobs=-1)
+        kpca = KernelPCA(n_components=100)
+        rf = RandomForestRegressor(n_estimators=100)
+
+        self.reg = make_pipeline(rte, kpca, rf)
  
+
     def fit(self, X, y):
         self.reg.fit(X, y)
  
